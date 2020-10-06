9fe018ffd98303642dc3ba745433db0c0d8224e2
tom metge <tom@accident-prone.com>
2020-10-06 08:01:22 -0700

    Hello from Tom

--- src/reed/Main.java
+++ src/reed/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Sarah");
-
+        System.out.println("Hello Sarah, my name is Tom");
     }
 }
