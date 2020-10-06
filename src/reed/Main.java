16bace0eb48fddea5f310e0fb8b03a726d4a8822
sarah <sarah@DESKTOP-OAUA1DU>
2020-10-05 20:33:44 -0700

    testing undo

--- src/reed/Main.java
+++ src/reed/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Sarah");
+        System.out.println("I'm making some changes to test the undo");
     }
 }
